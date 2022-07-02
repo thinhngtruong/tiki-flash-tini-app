84edcf918b30dba53e30caaa7eb3ecf536a4e208
Thinh Nguyen <thinh.ngtruong@gmail.com>
2022-07-02 21:38:35 +0700

    First commit

---
+++ src/pages/index/index.ts
@@ -0,0 +1,25 @@
+/// <reference lib="dom" />
+// => fix missing setTimeout
+
+import clone from 'lodash-es/clone';
+
+interface HomeData {
+  value: string;
+  isLoading: boolean;
+  banners: Array<any>;
+}
+interface HomeDataCustomMethods {
+  parseDataToString(data: HomeData): string;
+}
+Page<HomeData, HomeDataCustomMethods>({
+  data: { value: 'is default value', isLoading: false, banners: [{ id: 1 },{ id: 2 }] },
+  // @ts-ignore ==> test ts ignore flag
+  onLoad(query = {}) {
+    setTimeout(() => {
+      this.setData(clone({ value: 'has been changed' }));
+    }, 1000);
+  },
+  parseDataToString(data: Object) {
+    return JSON.stringify(data, null, 2);
+  },
+});
